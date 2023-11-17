e374dd026e837d07a744e41d947d88924c320188
Farkhunda Odinaeva <odinaevafarkhunda@gmail.com>
2023-11-17 12:44:13 +0100

    add message Hi

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main (String[] args){
         Orange orange = new Orange();
         int x = 10;
+        int y = 5;
+        int sun = x + y;
+        System.out.println("Wi, there!");
 
     }
 
